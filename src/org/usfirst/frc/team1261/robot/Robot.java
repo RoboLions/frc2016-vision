d386de5a8fb63bb3673ce2e5f08dd2a64eeb9d91
techcrab101 <=>
2016-01-28 19:37:50 -0500

    Changed to command-based robot.

--- src/org/usfirst/frc/team1261/robot/Robot.java
+++ src/org/usfirst/frc/team1261/robot/Robot.java
@@ -1,74 +1,108 @@
+
 package org.usfirst.frc.team1261.robot;
 
-import java.io.IOException;
-import java.time.LocalDateTime;
-
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Servo;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import org.usfirst.frc.team1261.robot.commands.ExampleCommand;
+import org.usfirst.frc.team1261.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * This is a short sample program demonstrating how to use the basic throttle
- * mode of the new CAN Talon.
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
  */
 public class Robot extends IterativeRobot {
 
-	// CANTalon motor;
-	Servo motor;
-	// use at resolution 
-	//private final static String[] GRIP_ARGS = new String[] { "/usr/local/frc/JRE/bin/java", "-jar",
-	//		"/home/lvuser/grip.jar", "/home/lvuser/project.grip" };
+	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
+	public static OI oi;
 
-	public Robot() {
-		motor = new Servo(9);
-		// motor = new CANTalon(1); // Initialize the CanTalonSRX on device 1.
-		// table = NetworkTable.getTable("GRIP/myContoursReport");
-	}
+    Command autonomousCommand;
+    SendableChooser chooser;
 
-	public void robotInit() {
-	/*
-		System.out.println("Running " + String.join(" ", GRIP_ARGS));
-		try {
-			Runtime.getRuntime().exec(GRIP_ARGS);
-		} catch (IOException e) {
-			System.out.println("Exception");
-			e.printStackTrace();
-		}
-		*/
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    public void robotInit() {
+		oi = new OI();
+        chooser = new SendableChooser();
+        chooser.addDefault("Default Auto", new ExampleCommand());
+//        chooser.addObject("My Auto", new MyAutoCommand());
+        SmartDashboard.putData("Auto mode", chooser);
+    }
+	
+	/**
+     * This function is called once each time the robot enters Disabled mode.
+     * You can use it to reset any subsystem information you want to clear when
+	 * the robot is disabled.
+     */
+    public void disabledInit(){
+
+    }
+	
+	public void disabledPeriodic() {
+		Scheduler.getInstance().run();
 	}
 
 	/**
-	 * Runs the motor.
+	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
+	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
+	 * Dashboard, remove all of the chooser code and uncomment the getString code to get the auto name from the text box
+	 * below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional commands to the chooser code above (like the commented example)
+	 * or additional comparisons to the switch structure below with additional strings & commands.
 	 */
-	public void teleopPeriodic() {
-		double[] defaultValue = new double[] {};
-		double[] centerYArray;
-		double[] areaArray;
-		double centerY = 120;
-		double maxArea = 0;
-		NetworkTable table;
-		//while (isEnabled()) {
-			table = NetworkTable.getTable("GRIP/myContoursReport");
-			areaArray = table.getNumberArray("area", defaultValue);
-			centerYArray = table.getNumberArray("centerY", defaultValue);
-			
-			for (int i = 0; i < areaArray.length; i++) {
-				if (areaArray[i] > maxArea) {
-					maxArea = areaArray[i];
-					centerY = centerYArray[i];
-				}
-			}
-			//System.out.print(LocalDateTime.now().toString());
-			//System.out.print(height[0] + " ");
-			//for (double cx : centerX) {
-			//	System.out.print(cx + " ");
-			//}
-			//double angle =((height.length > 0 ? height[0] : 0)/215)*180;
-			//System.out.print("angle: " + angle);
-			System.out.print("centerY:" +  centerY);
-			//motor.setAngle(angle);
-			System.out.println();
-			
-	}
+    public void autonomousInit() {
+        autonomousCommand = (Command) chooser.getSelected();
+        
+		/* String autoSelected = SmartDashboard.getString("Auto Selector", "Default");
+		switch(autoSelected) {
+		case "My Auto":
+			autonomousCommand = new MyAutoCommand();
+			break;
+		case "Default Auto":
+		default:
+			autonomousCommand = new ExampleCommand();
+			break;
+		} */
+    	
+    	// schedule the autonomous command (example)
+        if (autonomousCommand != null) autonomousCommand.start();
+    }
+
+    /**
+     * This function is called periodically during autonomous
+     */
+    public void autonomousPeriodic() {
+        Scheduler.getInstance().run();
+    }
+
+    public void teleopInit() {
+		// This makes sure that the autonomous stops running when
+        // teleop starts running. If you want the autonomous to 
+        // continue until interrupted by another command, remove
+        // this line or comment it out.
+        if (autonomousCommand != null) autonomousCommand.cancel();
+    }
+
+    /**
+     * This function is called periodically during operator control
+     */
+    public void teleopPeriodic() {
+        Scheduler.getInstance().run();
+    }
+    
+    /**
+     * This function is called periodically during test mode
+     */
+    public void testPeriodic() {
+        LiveWindow.run();
+    }
 }
